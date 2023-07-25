1e3e868d1456e1a8b36b12fd66e719bf6d97f74c
Asem Edress <asem.edress@gmail.com>
2023-07-25 14:21:07 +0200

    adding auth, user and posts controllers. Post model and routes.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -39,7 +39,7 @@ export const register = async (req, res) => {
         res.status(201).json(savedUser);
 
     } catch (err ){
-        res.status(500).json({error: err.message })
+        res.status(500).json({error: err.message });
     }
 
 };
@@ -48,8 +48,18 @@ export const register = async (req, res) => {
 
 export const login = async(req, res) => {
     try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email: email});
+        if (!user) return res.status(400).json({msg: "User does not exist"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg: "Invalid Credintials"});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET); 
+        delete user.password;
+        res.status(200).json({token, user})
 
     } catch (err){
-        
+        res.status(500).json({error: err.message })
     }
 }
