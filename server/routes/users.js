57b74964ae10e240060083385c32ce8d9f5f2eec
Asem Edress <asem.edress@gmail.com>
2023-07-27 22:27:27 +0200

    added .env on the other machine

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {
-    getUser,
-    getUserFriends,
-    addRemoveFriend,
-} from "../controllers/users.js"
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -16,6 +16,5 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 /* UPDATE */
 
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
-
 
 export default router;
